7e5dc60b7000e81eb1ed50e8dd79b2fd5869888a
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2023-04-16 08:54:13 +0200

    Successfully setup firebase hook and retrieve data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import './App.css';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase";
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [quotes, setQuotes] = useState([]);
+
+  const fetchQuotes = async () => {
+    setLoading(true);
+    await getDocs(collection(db, "quotes")).then((querySnapshot) => {
+      const newData = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setLoading(false);
+      setQuotes(newData);
+      console.log(quotes, newData);
+    });
+  };
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Retrieving quotes..
-        </p>
+      <header>
+        <h2>{loading ? 'Retrieving quotes..' : 'Quotes'}</h2>
       </header>
+      <ul>
+        {quotes.map((quote, index) => {
+          return (
+            <li key={index}>
+              <blockquote>
+                <i>{quote.block}</i>
+                <br />
+                <cite>{quote.cite}</cite>
+              </blockquote>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }

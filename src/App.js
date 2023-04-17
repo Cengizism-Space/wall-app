2d02491f03c642d8e846b4b5ab550e81417af6b6
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2023-04-17 18:23:35 +0200

    Eliminate firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "./firebase";
 import { useState, useEffect, useCallback } from "react";
 import "./App.css";
 
 function App() {
-  const [loading, setLoading] = useState(true);
   const [quotes, setQuotes] = useState([]);
 
   const fetchQuotes = useCallback(async () => {
-    setLoading(true);
-    await getDocs(collection(db, "quotes")).then((querySnapshot) => {
-      const newData = querySnapshot.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }));
-      setLoading(false);
-      setQuotes(newData);
-      console.log(quotes, newData);
-    });
-  }, [quotes]);
+    setQuotes([
+      {
+        block: "Simplicity is the ultimate sophistication.",
+        cite: "Leonardo da Vinci",
+      },
+      {
+        block: "Gravitation is not the reason for falling in love.",
+        cite: "Albert Einstein",
+      },
+    ]);
+  }, []);
 
   useEffect(() => {
     fetchQuotes();
@@ -26,9 +23,6 @@ function App() {
 
   return (
     <div className="App">
-      <header>
-        <h2>{loading ? 'Retrieving quotes..' : 'Quotes'}</h2>
-      </header>
       <ul>
         {quotes.map((quote, index) => {
           return (
